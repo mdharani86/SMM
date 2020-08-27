3d1b5e2982607622495656ef3af270cf664d156b
mdharani86 <69230820+mdharani86@users.noreply.github.com>
2020-08-27 16:57:12 -0400

    Update warmup.py

--- warmup.py
+++ warmup.py
@@ -1,59 +1,28 @@
+from itertools import combinations
 
 Node = {1:[3,4,5,6,8],2:[3,4,5,7,8],3:[1,2,7,4],4:[3,2,1,8],5:[1,2,6],6:[1,5],7:[2,3],8:[1,2,4]}
 
-active_set = {6, 7, 8}
-inactive_set = {1, 2, 3, 4, 5}
-print('Original Sets:')
-print(f'active set: {active_set}')
-print(f'inactive_set: {inactive_set}')
-print(f'connectivity: {Node}')
-for key in Node.keys():
-    count_active = 0
-    count_inactive = 0
-    ratio_list = []
-
-    for list_value in Node.get(key):
-        if list_value in active_set:
-            count_active += 1
-        else:
-            count_inactive += 1
-    if key in inactive_set:
-        ratio = count_active / (count_active + count_inactive)
-        ratio_list.append(ratio)
-        # print()
-        print(f'Inactive Node: {key}, active nodes connected:{count_active}, inactive nodes connected:{count_inactive}, percent active:{ratio}')
-        # print()
-
+list = [1,2,3,4,5,6,7,8]
+active = combinations(list,3)
 set_cnt = 0
-for i in active_set:
-    for j in inactive_set:
-        # swapping i and j between active and inactive sets
-        temp_active = active_set.copy()
-        temp_inactive = inactive_set.copy()
-        temp_i = i
-        temp_j = j
-        temp_active.remove(temp_i)
-        temp_inactive.remove(temp_j)
-        temp_active.add(temp_j)
-        temp_inactive.add(temp_i)
-        set_cnt +=1
-        print()
-        print(f'Swapped set {set_cnt}, Swapped Nodes: {i},{j}')
-        print(temp_active)
-        print(temp_inactive)
-        print(Node)
-        print()
-        ratio_list = []
+for active_tuple in active:
+    set_cnt +=1
+    active_list = [item for item in active_tuple]
+    print(f'set count:{set_cnt}')
+    print(f'active_set: {active_list}')
+    inactive_list = [item for item in list if item not in active_list]
+    print(f'inactive_list: {inactive_list}')
+    print(f'Connections: {Node}')
 
-        for key in Node.keys():
-            count_active = 0
-            count_inactive = 0
-
-            for list_value in Node.get(key):
-                if list_value in temp_active:
-                    count_active +=1
-                else:
-                    count_inactive +=1
-            if key in temp_inactive:
-                ratio = count_active/(count_active + count_inactive)
-                print(f'Inactive Node: {key}, active Nodes connected:{count_active}, inactive active nodes connected:{count_inactive}, active percent:{ratio}')
+    for key in Node.keys():
+        count_active = 0
+        count_inactive = 0
+        for list_value in Node.get(key):
+            if list_value in active_list:
+                count_active +=1
+            else:
+                count_inactive +=1
+        if key in  inactive_list:
+            ratio = count_active/(count_active + count_inactive)
+            print(f'Inactive Node#: {key}, active neighbours count:{count_active}, inactive neighbours count:{count_inactive}, active percent:{ratio}')
+    print()
